30fb66d5a6fbf080b40b9134da14948a610682df
Astha Paroha <asthaparoha6@gmail.com>
2026-05-18 16:42:29 +0530

    Added Java training sessions again in main branch

---
+++ Astha_java_training/session4/todoapp/src/test/java/com/astha/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astha.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
